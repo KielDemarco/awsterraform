f9295365298e6ed56e4db296f6c1a1eb6aa2762d
Kiel DeMarco <73503331+KielDemarco@users.noreply.github.com>
2021-03-25 00:58:40 -0700

    Update maint.tf

--- cloud9/maint.tf
+++ cloud9/maint.tf
@@ -10,6 +10,6 @@ resource "aws_cloud9_environment_ec2" "CloudScrap" {
   instance_type               = lookup(var.t2_instances, "t2_micro", "t2.micro")
   name                        = "CloudScrap"
   automatic_stop_time_minutes = var.a-stop-time
-  subnet_id                   = "subnet-0148681f4c63a096c"
+  subnet_id                   = #enter subnet here.
 }
 
