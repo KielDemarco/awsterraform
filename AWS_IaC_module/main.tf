7967e1eddfb0d3005352785e81c06f9f96dc699c
Kiel DeMarco <73503331+KielDemarco@users.noreply.github.com>
2021-03-27 06:04:47 -0700

    Update main.tf

--- AWS_IaC_module/main.tf
+++ AWS_IaC_module/main.tf
@@ -10,6 +10,8 @@ module "ec2_module" {
   ami          = var.ami
   t2_instances = var.t2_instances
   m4_instances = var.m4_instances
+  ec2_count    = var.ec2_count_i1
+  ec2_count_2  = var.ec2_count_i2
   region       = var.region
   pub_sub      = module.vpc_module.pub_sub_id
   sec_group    = module.vpc_module.security_group_output
