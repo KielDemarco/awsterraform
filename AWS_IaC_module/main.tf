251fedd7d192064cb9758a0a1f6c847c77e5e9b5
KielSD <73503331+kielSDeM@users.noreply.github.com>
2022-04-27 06:59:48 -0600

    Update main.tf

--- AWS_IaC_module/main.tf
+++ AWS_IaC_module/main.tf
@@ -1,12 +1,12 @@
 module "vpc_module" {
-  source       = "../AWS_module/vpc_module"
+  source       = "../AWS_IaC_module/vpc_module"
   vpc_cidr     = var.vpc_cidr
   public_cidr  = var.public_cidr
   private_cidr = var.private_cidr
   region       = var.region
 }
 module "ec2_module" {
-  source       = "../AWS_module/ec2_module"
+  source       = "../AWS_IaC_module/ec2_module"
   ami          = var.ami
   t2_instances = var.t2_instances
   m4_instances = var.m4_instances
